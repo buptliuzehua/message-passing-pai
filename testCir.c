d3c96366ecd887aa574efb9852dee267c23cafc9
LiuZehua <s1920176@ed.ac.uk>
2019-10-01 15:51:17 +0100

    123

--- testCir.c
+++ testCir.c
@@ -66,7 +66,7 @@ int main(void)
       result+=temp;
       //printf("%f\n", result);
     }
-    result = final_deal_pai(result,num_in_each_pro，size-1);
+    result = final_deal_pai(result,num_in_each_pro);
     //printf("%f\n", result);
     MPI_Barrier(MPI_COMM_WORLD);
 
